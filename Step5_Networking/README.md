8b67206cfef663154000140b39fdb9ae6d855eed
teddy0 @ archbootstrap <tong011@e.ntu.edu.sg>
2022-06-16 23:56:39 -0700

    trying out networking scripts

---
+++ Step5_Networking/README.md
@@ -0,0 +1,11 @@
+# Networking
+
+Setting up networking, while not essential for barebones arch, 
+is essential for getting arch usable.
+
+Without networking, you cannot run pacman or install stuff
+from the AUR. You cannot use git etc.
+
+IMO, i consider it essential.
+
+## hosts file
---
+++ Step5_Networking/networkingScripts.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+
+# this file is meant to be sourced, and then you have many functions with
+# which to install arch
+
+# for this file, you will be executing commands in the 
+# chroot environment
+
+# the chrootExec function
+# executes a command in the chroot environment
+# assuming the chroot is at /mnt
+# credit to:
+# https://askubuntu.com/questions/551195/scripting-chroot-how-to
+
+echo "NOTE: scripts are meant to be run from bootstrap, NOT chrooted"
+echo "in your installation!"
+
+echo " "
+echo " "
+function chrootExec {
+	arch-chroot /mnt $1
+}
+
+echo "chrootExec [COMMAND] --> execute command from chroot"
+
+function addHostName {
+
+	# first clean up the hostname file, 
+	# then put the hostname there
+	rm -rf /mnt/etc/hostname
+	touch /mnt/etc/hostname
+	echo $1 >> /mnt/etc/hostname
+
+	echo " "
+	echo "on /mnt/etc/hostname:"
+	cat /mnt/etc/hostname
+
+	# next is /etc/hosts
+	rm -rf /mnt/etc/hosts
+	touch /mnt/etc/hosts
+	echo "127.0.0.1		localhost" >> /mnt/etc/hosts
+	echo "::1			localhost" >> /mnt/etc/hosts
+	echo "127.0.0.1		$1" >> /mnt/etc/hosts
+
+	echo " "
+	echo "on /mnt/etc/hosts:"
+	cat /mnt/etc/hosts
+
+}
+
+echo "addHostName [hostname] --> add hostname to /etc/hostname and /etc/hosts"
+echo "warning: will overwrite any existing file"
+
+function addSambaDNS {
+	pacstrap /mnt samba --needed --noconfirm
+	chrootExec "systemctl enable nmb"
+}
+
+
+echo "addSambaDNS --> enable microsoft netbios hostname resolution,"
+echo "Note: installs samba"
+
+function addNetworkManager {
+	pacstrap /mnt networkmanager --needed --noconfirm
+	chrootExec "systemctl enable NetworkManager"
+}
+
+echo "addNetworkManager --> adds the network manager cli and enables it with"
+echo "systemctl enable"
+
+function addBlueTooth {
+	pacstrapInstallToMnt bluez
+
+}
+echo "addBlueTooth --> adds bluetooth package"
+echo "systemctl enable"
+
+
+# with a network setup, you can autoupdate mirrors
+# using a reflector service
+function reflectorSetup {
+	pacstrapInstallToMnt reflector
+	echo "--country United States, Singapore, France, Germany" >> /mnt/etc/xdg/reflector/reflector.conf
+	chrootExec "systemctl enable reflector"
+}
+
+echo "reflectorSetup --> installs reflector "
+echo "also adds the reflector service to autoupdate mirrors"
+echo "my specific script is United States, Singapore, France, Germany"
+
+function dhcpSetup {
+	pacstrapInstallToMnt dhcpcd
+	chrootExec "systemctl enable dhcpcd"
+}
+
+echo "dhcpSetup --> sets up dhcp client daemon for dyanmic IP address"
+
+function pacstrapInstallToMnt {
+	pacstrap /mnt $1 --needed --noconfirm
+}
