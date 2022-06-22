435fce91e6aa2d9bfc6e81a7bbfbec0a1009cd64
teddy0 @ archbootstrap <tong011@e.ntu.edu.sg>
2022-06-21 21:29:58 -0700

    first iteration of KDE pacstrap installed

---
+++ Step7_GUI/installGUI.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+
+# this file is meant to be sourced, and then you have many functions with
+# which to install arch
+
+# for this file, you will be executing commands in the 
+# chroot environment
+
+# the chrootExec function
+# executes a command in the chroot environment
+# assuming the chroot is at /mnt
+# credit to:
+# https://askubuntu.com/questions/551195/scripting-chroot-how-to
+
+echo "NOTE: scripts are meant to be run from bootstrap, NOT chrooted"
+echo "in your installation!"
+
+echo " "
+echo " "
+function chrootExec {
+	arch-chroot /mnt $1
+}
+
+echo "chrootExec [COMMAND] --> execute command from chroot"
+
+function InstallDrivers {
+
+	pacman -Ss xf86-video
+	pacstrapInstall xorg-drivers
+
+
+}
+
+echo "InstallDrivers --> displays and installs xorg-drivers"
+
+function InstallXorg {
+	pacstrapInstall xorg xorg-apps
+
+}
+echo "InstallXorg --> installs xorg-apps and xorg to /mnt"
+
+function InstallKDE {
+	InstallDrivers
+	InstallXorg
+	pacstrapInstall plasma-meta kde-applications
+	pacstrapInstall sddm sddm-kcm
+	chrootExec "systemctl enable sddm"
+}
+
+echo "InstallKDE --> installs KDE-plasma and enables sddm service in systemctl"
+
+
+
+
+function pacstrapInstall {
+
+	pacstrap /mnt $1 --needed --noconfirm
+}
