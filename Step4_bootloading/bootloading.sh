78b1fcf73e751c3200533e316007711d5aea01a4
teddyRootManjaro <tong011@e.ntu.edu.sg>
2022-06-25 12:18:15 -0700

    added comments for OSprober and how to use it properly

--- Step4_bootloading/bootloading.sh
+++ Step4_bootloading/bootloading.sh
@@ -15,7 +15,10 @@ echo "autoInstallGrub --> installs grub onto /mnt/boot with EFI"
 # https://www.atulhost.com/how-to-disable-or-enable-os-prober-from-the-grub
 function enableOSProber {
 	sed -i 's/#GRUB_DISABLE_OS_PROBER=false/GRUB_DISABLEP_OS_PROBER=false/g' /mnt/etc/default/grub
+	vim /mnt/etc/default/grub
 	arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 }
 
-echo "new function added: enableOSProber [TBD] not really working yet"
+echo "new function added: enableOSProber --> enables OS prober in grub"
+echo "ensure to mount all the filesystems you want OSprober to work"
+echo "on BEFORE using enableOSProber"
