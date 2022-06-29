c9dd4e996cc0daa55cfe34eb70d21876ad55c676
teddy0@archDesktop <tong011@e.ntu.edu.sg>
2022-06-28 22:36:40 -0700

    obs install script included

---
+++ Config6_productivity/productivity.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+function obsStudioInstall {
+
+	pacInstall obs-studio
+	paruInstall obs-ndi
+	pacInstall ffnvcodec-headers
+}
+
+echo "obsStudioInstall --> installs obs-studio and some other"
+echo "useful packages for obs"
+
+function pacInstall {
+	sudo pacman -S $@ --needed --noconfirm
+}
+
+
+echo "pacInstall [packages] --> executes sudo pacman -S --needed --noconfirm"
+
+function paruInstall {
+	paru -S $@ --needed --noconfirm
+}
+echo "paruInstall [packages] --> executes paru -S --needed --noconfirm"
