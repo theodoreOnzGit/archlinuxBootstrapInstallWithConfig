ff3e3215868f0c6bd9f03816fb30356ad348266f
teddy0@archbootstrap <tong011@e.ntu.edu.sg>
2022-07-01 05:00:11 +0000

    autoInstallGrub patched so that chroot script works

--- Step4_bootloading/bootloading.sh
+++ Step4_bootloading/bootloading.sh
@@ -7,7 +7,12 @@
 function autoInstallGrub {
 	#mount $1 /mnt/boot
 	grub-install --target=x86_64-efi --efi-directory=/mnt/boot --bootloader-id=archGRUB --boot-directory=/mnt/boot --removable
-	arch-chroot /mnt "grub-mkconfig -o /boot/grub/grub.cfg"
+	touch grubMkconfig.sh
+	chmod 755 grubMkconfig.sh
+	echo "grub-mkconfig -o /boot/grub/grub.cfg" >> grubMkconfig.sh
+	mv grubMkconfig.sh /mnt/root/grubMkconfig.sh
+	chrootExec /root/grubMkconfig.sh
+	rm -rf /mnt/root/grubMkconfig.sh
 }
 
 echo "autoInstallGrub --> installs grub onto /mnt/boot with EFI"
@@ -22,3 +27,14 @@ function enableOSProber {
 echo "new function added: enableOSProber --> enables OS prober in grub"
 echo "ensure to mount all the filesystems you want OSprober to work"
 echo "on BEFORE using enableOSProber"
+
+function installBiosBoot {
+	grub-install --target=i386-pc $1 --recheck
+}
+echo "[incomplete] installBiosBoot [/dev/sdX] --> installs a grub boot entry into the BIOS"
+
+function chrootExec {
+	arch-chroot /mnt $1
+}
+
+echo "chrootExec [command] --> executes command as root on /mnt"
