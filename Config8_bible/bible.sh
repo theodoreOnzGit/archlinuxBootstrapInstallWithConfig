5d04095dd904ebd71ef93f1d73b7abf200d965f2
teddy0@archDesktop <tong011@e.ntu.edu.sg>
2022-07-18 01:07:54 -0700

    bible apps added

---
+++ Config8_bible/bible.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+bibleAppsInstall() {
+	paruInstall xiphos \
+		bibletime
+}
+
+
+function pacInstall {
+	sudo pacman -S $@ --needed --noconfirm
+}
+
+
+echo "pacInstall [packages] --> executes sudo pacman -S --needed --noconfirm"
+
+function paruInstall {
+	paru -S $@ --needed --noconfirm
+}
+echo "paruInstall [packages] --> executes paru -S --needed --noconfirm"
