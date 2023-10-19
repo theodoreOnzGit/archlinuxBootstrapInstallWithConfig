ffc021f5e741314bf59d626e3f1764b2bdbff339
teddy0@archDesktop <tong011@e.ntu.edu.sg>
2023-10-19 19:09:19 +0800

    added openblas install to scientific computing

--- Config1_developmentEnvironment/scientificComputing.sh
+++ Config1_developmentEnvironment/scientificComputing.sh
@@ -4,24 +4,30 @@
 # which to install arch
 
 # this installs grub on /mnt/boot
+
+echo "coolPropPrereqsInstall --> installs mono and etc. for "
+echo "for compiling C# compliant shared object .so files \n"
 function coolPropPrereqsInstall() {
 	pacInstall mono cython python swig \
 		python2-six p7zip python2
 	# http://www.coolprop.org/coolprop/wrappers/Csharp/index.html
 }
 
-echo "coolPropPrereqsInstall --> installs mono and etc. for "
-echo "for compiling C# compliant shared object .so files \n"
+
+echo "openfoam_third_party --> Installs boost cgal fftw openmpi"
+echo "paraview utf8cpp from arch official repos"
+echo "and uses paru to install parmetis and scotch"
+echo "these are third party packages required for openfoam-com \n"
 
 function openfoam_third_party {
 	pacInstall boost cgal fftw gcc openmpi paraview utf8cpp
 	paruInstall parmetis scotch
 }
 
-echo "openfoam_third_party --> Installs boost cgal fftw openmpi"
-echo "paraview utf8cpp from arch official repos"
-echo "and uses paru to install parmetis and scotch"
-echo "these are third party packages required for openfoam-com \n"
+echo "openblas_install: installs openblas"
+function openblas_install(){
+	pacInstall openblas
+}
 
 
 function pacInstall {
