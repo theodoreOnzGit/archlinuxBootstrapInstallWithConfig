2d9557834c914aeda6b89a9c1fad3692ead73b9b
teddy0@arch-desktop-aftershock <theodore_ong@berkeley.edu>
2026-01-07 10:03:01 +0800

    removed lsp config for lua server for nvim v0.11

--- Config1_developmentEnvironment/nvim_files/lua/lsp.lua
+++ Config1_developmentEnvironment/nvim_files/lua/lsp.lua
@@ -4,8 +4,8 @@ lsp.on_attach(function(client, bufnr)
 	lsp.default_keymaps({ buffer = bufnr })
 end)
 
--- (Optional) Configure lua language server for neovim
-require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+-- (Deprecated and Optional) Configure lua language server for neovim
+-- require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
 lsp.setup()
 
